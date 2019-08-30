0d8ba70d9ce90203bdb617bae726808c2ba9eb49
Steven Decker <swdecker@gmail.com>
2019-08-30 23:23:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
 # My Code here....
+def map_to_negativize(arr)
+  arr_to_ret = []
+  arr.length.times do |i|
+    arr_to_ret.push(arr[i]*-1)
+  end
+  arr_to_ret
+end
+
+def map_to_no_change(arr)
+  arr_to_ret = []
+  arr.length.times do |i|
+    arr_to_ret.push(arr[i])
+  end
+  arr_to_ret
+end
+
+def map_to_double(arr)
+  arr_to_ret = []
+  arr.length.times do |i|
+    arr_to_ret.push(arr[i]*2)
+  end
+  arr_to_ret
+end
+
+def map_to_square(arr)
+  arr_to_ret=[]
+  arr.length.times do |i|
+    arr_to_ret.push(arr[i]**2)
+  end
+  arr_to_ret
+end
+
